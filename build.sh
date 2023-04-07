73ef1c457c2a0d2d4569a834981569b41f63a02e
charckle <tsobw@email.com>
2023-04-07 07:53:46 +0200

    version 1.1.19

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 # docker hub username
-docker build -t $USERNAME/$IMAGE:$version .
-docker image tag $USERNAME/$IMAGE:$version $USERNAME/$IMAGE:latest
+echo "building docker images for version $version"
+echo "building docker images for version $BANANA"
+echo "building docker images for version $USERNAME"
+#docker build -t $USERNAME/$IMAGE:$version .
+#docker image tag $USERNAME/$IMAGE:$version $USERNAME/$IMAGE:latest
 
 # push it to docker
-docker push $USERNAME/$IMAGE:latest
-docker push $USERNAME/$IMAGE:$version
+#docker push $USERNAME/$IMAGE:latest
+#docker push $USERNAME/$IMAGE:$version
--- release.sh
+++ release.sh
@@ -2,20 +2,24 @@
 
 # docker credentials
 USERNAME=charckle
+BANANA=banana
 IMAGE=yt-dlp
 
+echo "pulling latest image from git"
 git pull
 
 # bump version
+OLD_VERSION=$(cat VERSION)
+echo "Bumping version from $OLD_VERSION"
 docker run --rm -v "$PWD":/app treeder/bump patch
-version=`cat VERSION`
+version=$(cat VERSION)
 echo "version: $version"
 
 # now run build
-./build.sh
+. build.sh
 
 # now make tags and push it to git
-
+cat "Adding tags to git and pushing"
 git add .
 git commit -m "version $version"
 git push
