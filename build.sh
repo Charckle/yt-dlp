8479dacffb45b38d8bc686cf8db338afb1582742
charckle <tsobw@email.com>
2023-04-08 18:06:39 +0200

    version 1.1.20

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 # docker hub username
 docker build -t $USERNAME/$IMAGE:$version .
 docker image tag $USERNAME/$IMAGE:$version $USERNAME/$IMAGE:latest
-
 # push it to docker
 docker push $USERNAME/$IMAGE:latest
 docker push $USERNAME/$IMAGE:$version
--- release.sh
+++ release.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
+# -eE sets the bash environment tostop if any command gives a non 0 return
+# and launches the trap
+set -eE
+trap "echo Error!" ERR
 
 # docker credentials
 USERNAME=charckle
-BANANA=banana
 IMAGE=yt-dlp
 
 echo "pulling latest image from git"
-git pull
+#git pull
 
 # bump version
 OLD_VERSION=$(cat VERSION)
 echo "Bumping version from $OLD_VERSION"
-docker run --rm -v "$PWD":/app treeder/bump patch
+#docker run --rm -v "$PWD":/app treeder/bump patch
 version=$(cat VERSION)
 echo "version: $version"
 
 # now run build
+# build will create the latest image, and the version image, and push both to docker
 . build.sh
 
 # now make tags and push it to git
